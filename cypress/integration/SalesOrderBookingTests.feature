1097be48e45eb5da854b2ae1502e2d21de8b04fb
vankab <vankab@users.noreply.github.com>
2020-01-23 15:12:25 +0530

    changes

--- cypress/integration/SalesOrderBookingTests.feature
+++ cypress/integration/SalesOrderBookingTests.feature
@@ -2,11 +2,9 @@ Feature: Saled Order Booking Tests
 
    Saled Order Booking Tests
 
-Scenario: Login Validation
-Given I open "https://sab.elements-stage.cdkapps.eu/"
-When I enter valid credentials
-Then Title should be "Service Appointment Booking"
-
-Scenario: Test
-Given Test
-Then Print Test123
+Scenario: Scenario-1 
+# Given I open "https://sab.elements-stage.cdkapps.eu/"
+# When I enter valid credentials
+# Then Title should be "Service Appointment Booking"
+# Then Select Workshop, Vehicle Details, Booking Options, Services and Inspection
+And Select Date, Time and Confirm Booking
--- cypress/integration/SalesOrderBookingTests/salesOrderBookingTests.js
+++ cypress/integration/SalesOrderBookingTests/salesOrderBookingTests.js
@@ -1,29 +1,30 @@
-import { Given, When, Then } from "cypress-cucumber-preprocessor/steps"
+import { Given, When, Then, And } from "cypress-cucumber-preprocessor/steps"
 import { SalesOrderBooking, test } from "../Common/common.js"
 var data = require("../../fixtures/Testdata/testdata.json")
+
+var sob = new SalesOrderBooking();
 
 beforeEach(() => {
     cy.log('Im before each')
 })
 
-Given("Test",()=>{
-    cy.log("Test")
-})
-Then("Print Test123",()=>{
-    cy.log("Test123123123")
-})
 Given('I open {string}', (url) => {
     cy.visit(url);
 })
 When('I enter valid credentials', () => {
-    var sob = new SalesOrderBooking();
     sob.Login(data.uname, data.pwd);
 })
 Then('Title should be {string}', (title) => {
     cy.title().should('eq', title)
 })
+Then('Select Workshop, Vehicle Details, Booking Options, Services and Inspection', () => {
+    sob.SelectDetails(data.workshop, data.vehicle, data.bookingOption, data.services, data.inspection)
+})
+And('Select Date, Time and Confirm Booking',()=>{
+    var d = new Date()
+    console.log(d.getDate())
+})
 
-
-afterEach(()=>{
+afterEach(() => {
     cy.log('Im after each')
 })
